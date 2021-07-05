549d3ab2deba4f3efc645d0e9a1b78f887d54ebb
Abdhilabs <rizaabdhi@gmail.com>
2021-07-05 18:44:19 +0700

    Initial Commit

---
+++ Moneylabs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moneylabs
+//
+//  Created by Abdhi on 05/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
